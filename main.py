43fe91c6939730cef4886748da14684958836bb1
user1 <artvid29@gmail.com>
2023-09-28 14:26:33 +0300

    hiew

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hiei, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+    print(f'Hieiee, {name}')  # Press Ctrl+8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
